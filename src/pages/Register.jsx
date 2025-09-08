68874a0a6ad15f4e56aab540a3fafb5ae16b0a40
Gideon <kwarteon08@gmail.com>
2025-09-08 09:56:33 +0000

    done

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Add from "../img/addAvatar.png"
 
 const Register = () => {
   return (
@@ -9,13 +10,14 @@ const Register = () => {
                 <h1 className='font-bold text-2xl text-blue-900'>We Talk!</h1>
                 <p className='text-blue-900 text-sm'>Register</p>
 
-                <form className='flex flex-col gap-4'>
-                        <input className='p-3 border-b-1 border-b-blue-300 outline-none placeholder-gray-400 placeholder:text-sm' type="text" placeholder='name' />
-                        <input className='p-3 border-b-1 border-b-blue-300 outline-none placeholder-gray-400 placeholder:text-sm' type="email" placeholder='email' />
-                        <input className='p-3 border-b-1 border-b-blue-300 outline-none placeholder-gray-400 placeholder:text-sm' type="password" placeholder='password'/>
+                <form className='flex flex-col gap-4 '>
+                        <input className='p-3 border-b-1 focus:border-b-blue-400 border-b-gray-300 outline-none placeholder-gray-400 placeholder:text-sm w-[18rem]' type="text" placeholder='name' />
+                        <input className='p-3 focus:border-b-blue-400 border-b-1  border-gray-300 outline-none placeholder-gray-400 placeholder:text-sm' type="email" placeholder='email' />
+                        <input className='p-3 focus:border-b-blue-400 border-b-1 border-b-gray-300 outline-none placeholder-gray-400 placeholder:text-sm' type="password" placeholder='password'/>
                         <input type="file" id='file' style={{display:"none"}} />
-                        <label htmlFor="file">
-                                <img src="" alt="" />
+                        <label htmlFor="file" className='flex items-center gap-[10px] text-blue-900 cursor-pointer'>
+                                <img src={Add} alt="" className='w-[30px]' />
+                                <span className='text-sm'>Add Avatar</span>
                         </label>
                         <button className='bg-blue-300 p-[10px] font-medium cursor-pointer text-white rounded-md border-none'>Sign Up</button>
                 </form>
